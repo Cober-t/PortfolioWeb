4d3e73143cf8607b7e22c8b0cf78160882988b44
Cober-t <36416351+Cober-t@users.noreply.github.com>
2022-12-14 10:51:09 +0100

    Lights

--- webpack/src/script.js
+++ webpack/src/script.js
@@ -2,82 +2,13 @@ import './style.css'
 import * as THREE from 'three'
 import * as dat from 'dat.gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
-import { PointLight, TetrahedronGeometry } from 'three/src/Three'
+import { DirectionalLightHelper, MeshStandardMaterial, PointLight, RectAreaLight, SpotLightShadow } from 'three/src/Three'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper' 
 import gsap from 'gsap'
 
 // +++++++++++++++++++++++++++++++++++++++
 // +++  Scene
 const scene = new THREE.Scene();
-
-// +++++++++++++++++++++++++++++++++++++++
-// +++  Textures
-const textureLoader = new THREE.TextureLoader()
-const furAlbedo = textureLoader.load('/textures/fur/albedo.png')
-const furAO = textureLoader.load('/textures/fur/ambientOcclusion.png')
-const furHeight = textureLoader.load('/textures/fur/height.png')
-const furNormal = textureLoader.load('/textures/fur/normal.png')
-
-const rockWallAlbedo = textureLoader.load('/textures/rockWall/albedo.png')
-const rockWallAO = textureLoader.load('/textures/rockWall/ambientOcclusion.png')
-const rockWallHeight = textureLoader.load('/textures/rockWall/height.png')
-const rockWallNormal = textureLoader.load('/textures/rockWall/normal.png')
-
-const bamboAlbedo = textureLoader.load('/textures/bamboWood/albedo.png')
-const bamboAO = textureLoader.load('/textures/bamboWood/ambientOcclusion.png')
-const bamboRoughness = textureLoader.load('/textures/bamboWood/roughness.png')
-const bamboNormal = textureLoader.load('/textures/bamboWood/normal.png')
-
-const matcapTex = textureLoader.load('/textures/matcaps/7.png')
-matcapTex.minFilter = THREE.NearestFilter;
-matcapTex.magFilter = THREE.NearestFilter;
-matcapTex.generateMipmaps = false;  // If we are using NearestFilter
-
-// +++++++++++++++++++++++++++++++++++++++
-// +++  Cube Texture
-const cubeTexLoader = new THREE.CubeTextureLoader();
-// Must load in order
-const envMapTex = cubeTexLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
-])
-
-// +++++++++++++++++++++++++++++++++++++++
-// +++  Fonts
-const fontLoader = new FontLoader();
-const matcapMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTex });
-const textValue = 'Hello Three.js';
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) =>
-    {
-        const textGeometry = new TextGeometry (
-            textValue,
-            {
-                font:   font,
-                size:   0.5,
-                height: 0.2,
-                curveSegments: 4,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 3
-            }
-        )
-        textGeometry.computeBoundingBox();
-        textGeometry.center();
-
-        const text = new THREE.Mesh(textGeometry, matcapMaterial);
-        text.position.y = 1.5;
-        scene.add(text);
-    }
-);
 
 // +++++++++++++++++++++++++++++++++++++++
 // +++  Debug
@@ -112,152 +43,104 @@ gui.add(debugObject, 'fullscreen');
 
 // +++++++++++++++++++++++++++++++++++++++
 // +++  Lights
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.8);
-const pointLight = new THREE.PointLight('#00ffff', 0.3);
-pointLight.position.set(-1.5, 1.5, 1.5);
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.25);
+scene.add(ambientLight)
+// Directional light
+const dirLight = new THREE.DirectionalLight('#00ff00', 0.6);
+dirLight.position.set(2.5, 0.25, 0);
+scene.add(dirLight)
+// Hemisphere light
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 1)
+scene.add(hemisphereLight);
+// Point light
+const pointLight = new THREE.PointLight('#0005ff', 0.35);
+pointLight.position.set(-0.65, -0.65, 1.5);
+scene.add(pointLight);
 gui.add(pointLight.position, 'x').min(-3).max(3).step(0.01);
 gui.add(pointLight.position, 'y').min(-3).max(3).step(0.01);
 gui.add(pointLight.position, 'z').min(-3).max(3).step(0.01);
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01);
 gui.add(pointLight, 'intensity').min(0).max(1).step(0.01);
+gui.add(pointLight, 'distance').min(1).max(10).step(0.01);
+gui.add(pointLight, 'decay').min(1).max(10).step(0.01);
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01);
 gui.addColor(debugObject, 'color')
 .onChange(() => {
     pointLight.color.set(debugObject.color);
 })
-const pointLightBox = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(),
-    new THREE.MeshBasicMaterial( {color: '#00ffff'})
-)
-pointLightBox.scale.set(0.05, 0.05, 0.05)
+// RectArea light (high cost)
+const rectAreaLight = new THREE.RectAreaLight(0xf000ff, 1, 3, 1.5);
+rectAreaLight.position.set(1, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+// Spot light (high cost)
+const spotLight = new THREE.SpotLight(0xffff00, 0.5, 6, Math.PI * 0.1, 0.25, 1);
+scene.add(spotLight);
+spotLight.position.x = -1;
+spotLight.target.position.x = -0.25;
+scene.add(spotLight.target);
 
-// +++++++++++++++++++++++++++++++++++++++
-// +++  Materials
-//////  Albedo
-// const material = new THREE.MeshBasicMaterial({color: 'red'});
-// material.flatShading = true;
-//////  Normal
-//const material = new THREE.MeshNormalMaterial()
-//////  Matcap
-// const material = new THREE.MeshMatcapMaterial();
-// material.matcap = matcapTex;
-//////  Depth
-// const material = new THREE.MeshDepthMaterial();
-// material.matcap = matcapTex;
-//////  Lambert (reactive to lights)
-// const material = new THREE.MeshLambertMaterial();
-//////  Phong (reactive to lights)
-//const material = new THREE.MeshPhongMaterial(); 
-//material.shininess = 10;
-//material.specular = new THREE.Color('yellow');
-//////  Toon (reactive to lights)
-// const material = new THREE.MeshToonMaterial();
-// material.gradientMap = gradientTexture;
-//////  Standard (the good one, support light, metalness and roughness)
-const furMat = new THREE.MeshStandardMaterial();
-gui.add(furMat, 'metalness').min(0).max(1).step(0.001);
-gui.add(furMat, 'roughness').min(0).max(1).step(0.001);
-gui.add(furMat, 'wireframe');
-gui.add(furMat, 'displacementScale').min(0).max(1).step(0.01);
-furMat.map = furAlbedo;
-furMat.aoMap = furAO;
-furMat.aoMapIntensity = 1;
-furMat.displacementMap = furHeight;
-furMat.displacementScale = 0.3
-// furMat.metalnessMap = 
-furMat.normalMap = furNormal;
-// furMat.normalScale.set(0.1, 0.1);
-// furMat.transparent = true
-// furMat.alphaMap = 
-furMat.side = THREE.DoubleSide;
-//////  Physic Material
-//////  Points Material (for particles)
-//////  Shader Material (custom)
+// Light helpers
+const groupLightHelpers = new THREE.Group();
+const directionalLightHelper = new THREE.DirectionalLightHelper(dirLight, 0.1);
+scene.add(directionalLightHelper)
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.1);
+scene.add(hemisphereLightHelper);
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.1);
+scene.add(pointLightHelper);
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+window.requestAnimationFrame(() =>
+{
+    spotLightHelper.update();
+})
+scene.add(spotLightHelper);
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+scene.add(rectAreaLightHelper);
 
-const bamboMat = new THREE.MeshStandardMaterial();
-bamboMat.metalness = 0.7;
-bamboMat.roughness = 0.0;
-//bamboMat.map = bamboAlbedo;
-//bamboMat.aoMap = bamboAO;
-//bamboMat.normalMap = bamboNormal;
-//bamboMat.roughness = bamboRoughness;
-bamboMat.envMap = envMapTex;
-
-const rockWallMat = new THREE.MeshStandardMaterial();
-gui.add(rockWallMat, 'displacementScale').min(0).max(1).step(0.01);
-rockWallMat.map = rockWallAlbedo;
-rockWallMat.aoMap = rockWallAO;
-rockWallMat.aoMapIntensity = 1;
-rockWallMat.displacementMap = rockWallHeight;
-rockWallMat.displacementScale = 0.08
-rockWallMat.normalMap = rockWallNormal;
-
-// +++++++++++++++++++++++++++++++++++++++
-// +++  Axis helper
-const axesHelper = new THREE.AxesHelper();
+groupLightHelpers.add(directionalLightHelper, hemisphereLightHelper, pointLightHelper, spotLightHelper, rectAreaLightHelper);
+scene.add(groupLightHelpers);
+gui.add(groupLightHelpers, 'visible');
 
 // +++++++++++++++++++++++++++++++++++++++
 // +++  Geometry
 const group = new THREE.Group();
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.2;
+gui.add(material, 'roughness').min(0).max(1).step(0.01);
 const sphere = new THREE.Mesh(
     new THREE.SphereBufferGeometry(0.5, 62, 62),
-    rockWallMat
+    material
 )
 sphere.position.x = -1.5
 group.add(sphere);
     
 const torus = new THREE.Mesh(
     new THREE.TorusBufferGeometry(0.4, 0.3, 64, 128),
-    bamboMat
+    material
 )
 torus.position.x = 1.5
 
 group.add(torus);
-        
+
+const planeMat = new MeshStandardMaterial();
+planeMat.side = THREE.DoubleSide;
 const plane = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(1, 1, 100, 100),
-    furMat
+    new THREE.PlaneBufferGeometry(1, 1),
+    planeMat
 )
 group.add(plane);
-// UVs
-torus.geometry.setAttribute(
-    'uv2', 
-    new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
-);
-sphere.geometry.setAttribute(
-    'uv2', 
-    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
-);
-plane.geometry.setAttribute(
-    'uv2', 
-    new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
-);
+        
+const floor = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1, 1),
+    material
+)
+floor.scale.set(5, 5)
+floor.position.y = -1
+floor.rotation.x = -Math.PI/2;
+group.add(floor);
 
 scene.add(group);
-scene.add(ambientLight, pointLight, pointLightBox);
-scene.add(axesHelper);
 gui.add(group, 'visible');
-
-/////////////////////////////////////////
-//////////   100 donuts test  ///////////
-console.time('donuts');
-const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
-
-for (let i = 0; i < 1000; i++){
-    const donut = new THREE.Mesh(donutGeometry, matcapMaterial);
-
-    donut.position.x = (Math.random() - 0.5) * 10;
-    donut.position.y = (Math.random() - 0.5) * 10;
-    donut.position.z = (Math.random() - 0.5) * 10;
-
-    donut.rotation.x = Math.random() * Math.PI;
-    donut.rotation.y = Math.random() * Math.PI;
-
-    const randomScale = Math.random() * 0.1;
-    donut.scale.set(randomScale, randomScale, randomScale);
-    
-    scene.add(donut);
-}
-console.timeEnd('donuts');
 
 // +++++++++++++++++++++++++++++++++++++++
 // +++  Renderer
@@ -308,10 +191,6 @@ const tick = () => {
     
     // UPDATE Time
     const elapsedTime = clock.getElapsedTime();
-
-    // UPDATE Render
-    pointLightBox.material.color = pointLight.color
-    pointLightBox.position.set(pointLight.position.x, pointLight.position.y, pointLight.position.z)
 
     sphere.rotation.y = 0.3 * elapsedTime
     torus.rotation.y = 0.3 * elapsedTime
